3001eb1ff5c1a150e0700bcb9e35ea23d8efdf65
Daryll <gonsquared@gmail.com>
2025-07-31 18:52:40 +0800

    dockerize backend application

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
-from fastapi import FastAPI
 
-MONGO_URL = "mongodb://localhost:27017"
-MONGO_DB_NAME = "backendapp"
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "backendapp")
 
 client = AsyncIOMotorClient(MONGO_URL)
-db = client[MONGO_DB_NAME]
+db = client[DB_NAME]
